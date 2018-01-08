074a76e273c20e90a769f1c34737c2912288e646
danny128970878 <danny128970878@gmail.com>
2018-01-08 21:37:12 +0800

    Initial commit

---
+++ Assets/anim.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anim : MonoBehaviour {
+    public GameObject f;
+    public Animator ani;
+    // Use this for initialization
+    void Start () {
+        //ani = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            f.SetActive(true);
+            ani.SetBool("next", true);
+        }
+	}
+}
---
+++ Assets/tack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tack : MonoBehaviour {
+
+    //public GameObject sp;
+    //public ParticleSystem system;
+    public Transform enemy1;
+    public GameObject enemy2;
+    public float Walkdistance = 10;
+    public AudioSource a;
+
+    // Use this for initialization
+    void Start () {
+        // system.Stop();
+        a.Stop();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float distance = Vector3.Distance(this.transform.position, enemy1.transform.position);
+
+        if (distance <= Walkdistance)
+        {
+            //  system.Play();
+            enemy2.SetActive(true);
+            a.Play();
+        }
+    }
+}
